2f1ccd80657f50541e5af0769f4d41bbc4bc02d6
bseixeiro <benjamin.seixeiro@ecole-isitech.fr>
2024-08-26 11:21:03 +0200

    test sign gpg

--- app/_layout.js
+++ app/_layout.js
@@ -40,7 +40,7 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> 
-      <Stack.Screen name="+not-found" />
+      <Stack.Screen name="+not-found" options={{headerShown: true}} />
     </Stack>
   );
 }
